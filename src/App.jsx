aa9216a5150a08ab2508b254b07208b555243e7b
pablodru <pdrocha03@gmail.com>
2023-12-13 21:21:39 -0300

    chore: creating routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import CheckoutPage from "./pages/CheckoutPage"
+import KitchenPage from "./pages/KitchenPage"
+import OrdersPage from "./pages/OrdersPage"
+import ProductsPage from "./pages/ProductsPage"
+
 function App() {
 
   return (
     <>
-    </>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<ProductsPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/kitchen" element={<KitchenPage />} />
+          <Route path="/orders" element={<OrdersPage />} />
+				</Routes>
+			</BrowserRouter>
+		</>
   )
 }
 

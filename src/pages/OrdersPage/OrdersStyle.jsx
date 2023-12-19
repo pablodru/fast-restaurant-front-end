8e6649b356d80aea048206489baa9769bd3fba09
pablodru <pdrocha03@gmail.com>
2023-12-19 13:50:10 -0300

    style: responsive screen on Orders Page done

--- src/pages/OrdersPage/OrdersStyle.jsx
+++ src/pages/OrdersPage/OrdersStyle.jsx
@@ -1,25 +1,38 @@
 import { styled } from "styled-components";
 
 export const ScNameNotReady = styled.p`
-    font-family: "Roboto", sans-serif;
+	font-family: "Roboto", sans-serif;
 	font-size: 90px;
 	font-weight: 800;
 	color: #7e7b7b;
-    margin-bottom: 20px;
-`
+	margin-bottom: 20px;
+
+	@media (max-width: 485px) {
+		font-size: 46px;
+	}
+`;
 
 export const ScNameReady = styled.p`
-    font-family: "Roboto", sans-serif;
+	font-family: "Roboto", sans-serif;
 	font-size: 90px;
 	font-weight: 800;
 	color: #023d0a;
-    margin-bottom: 20px;
-`
+	margin-bottom: 20px;
+
+	@media (max-width: 485px) {
+		font-size: 46px;
+	}
+`;
 
 export const ScBorder = styled.div`
 	min-width: 5px;
 	min-height: 100%;
 	background-color: #000;
+
+	@media (max-width: 485px) {
+		min-width: 100%;
+		min-height: 5px;
+	}
 `;
 
 export const ScPage = styled.div`
@@ -27,6 +40,12 @@ export const ScPage = styled.div`
 	display: flex;
 	justify-content: space-between;
 	gap: 150px;
+
+	@media (max-width: 485px) {
+		padding: 20px 20px;
+		flex-direction: column;
+		gap: 30px;
+	}
 `;
 
 export const ScSide = styled.div`
@@ -35,6 +54,12 @@ export const ScSide = styled.div`
 	gap: 30px;
 	width: 50%;
 	height: 50%;
+
+	@media (max-width: 485px) {
+		width: 100%;
+		height: auto;
+		gap: 20px;
+	}
 `;
 
 export const ScPreparing = styled.p`
@@ -42,4 +67,8 @@ export const ScPreparing = styled.p`
 	font-size: 48px;
 	font-weight: 800;
 	color: #000;
+
+	@media (max-width: 485px) {
+		font-size: 28px;
+	}
 `;
